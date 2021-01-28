76169ff584e0e4cc5e64600a13265dac399ffd63
18557590 <books.antony@gmail.com>
2021-01-28 22:38:32 +0300

    Fixed bug with undefined props in CreateRoom

--- client/src/pages/CreateRoom.js
+++ client/src/pages/CreateRoom.js
@@ -1,10 +1,11 @@
+import { withRouter } from 'react-router-dom';
 import { v4 as uuid } from 'uuid';
 
-export const CreateRoom = (props) => {
+export const CreateRoom = withRouter((props) => {
   const create = () => {
     const roomId = uuid();
     props.history.push(`/room/${roomId}`);
   };
 
   return <button onClick={create}>Создайте комнату</button>;
-};
+});
