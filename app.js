5bb349db9a959d538a53cf25ad96cfad0dc92205
Anton <books.antony@gmail.com>
2021-02-09 00:23:08 +0300

    Some work with socket.io on a server side

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
+const io = require('socket.io')(server);
 const PORT = config.get('port') || 3000;
+
+const users = {};
+
+const socketToRoom = {};
 
 app.use(express.json({ extended: true }));
 
@@ -16,7 +23,52 @@ async function start() {
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    app.listen(PORT, () => {
+
+    io.on('connection', (socket) => {
+      console.log('Кто-то подключился');
+      socket.on('join room', (roomID) => {
+        if (users[roomID]) {
+          const length = users[roomID].length;
+          if (length === 4) {
+            socket.emit('room full');
+            return;
+          }
+          users[roomID].push(socket.id);
+        } else {
+          users[roomID] = [socket.id];
+        }
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter((id) => id !== socket.id);
+
+        socket.emit('all users', usersInThisRoom);
+      });
+
+      socket.on('sending signal', (payload) => {
+        io.to(payload.userToSignal).emit('user joined', {
+          signal: payload.signal,
+          callerID: payload.callerID,
+        });
+      });
+
+      socket.on('returning signal', (payload) => {
+        io.to(payload.callerID).emit('receiving returned signal', {
+          signal: payload.signal,
+          id: socket.id,
+        });
+      });
+
+      socket.on('disconnect', () => {
+        console.log('Кто-то отключился');
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+          room = room.filter((id) => id !== socket.id);
+          users[roomID] = room;
+        }
+      });
+    });
+
+    server.listen(PORT, () => {
       console.log(`App has been started on port ${PORT}...`);
     });
   } catch (e) {
